fbb7ae510bf885cc349c4370533f2645ab7d761e
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-09-09 09:33:21 +0530

    backend changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 // configurations
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: `${process.env.FRONTEND_URL}`,
         credentials: true,
     })
 );
