e7961f6f61bb5390211d5899c15040ba5b6067a7
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-09-09 22:52:22 +0530

    navbar

--- frontend/src/components/reusables/navbar.tsx
+++ frontend/src/components/reusables/navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useRecoilState } from "recoil";
 import { currentUser } from "@/utils/atom";
@@ -45,14 +45,16 @@ export default function Navbar({ className = "" }: NavbarProps) {
         <header
             className={`flex h-20 w-full shrink-0 items-center px-4 md:px-6 bg-gray-100 border-r border-gray-200 ${className}`}
         >
-            <Link
-                to="/"
+            <button
+                onClick={() => {
+                    navigate("/");
+                }}
                 className="flex flex-row justify-center items-center hidden lg:flex"
             >
                 <p className="font-extrabold leading-none tracking-tight text-2xl">
                     TICKOFF
                 </p>
-            </Link>
+            </button>
             <nav className="ml-auto hidden lg:flex gap-6">
                 <button
                     className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:scale-105 transition ease-in-out duration-300 disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50"
